8556f804fd49e6735f94d222928b3ffc746c4e38
Arthur d'Azémar <arthur.d-azemar@inria.fr>
2018-11-07 14:44:07 +0100

    add one test

--- smartwatts/test/test_tree.py
+++ smartwatts/test/test_tree.py
@@ -1,7 +1,7 @@
 import pytest
 
 from copy import deepcopy
-from smartwatts.tree import Node
+from smartwatts.utils.tree import Node
 
 
 class TestNode():
@@ -47,7 +47,8 @@ class TestNode():
         assert node_b.label == 'B'
         assert len(node_b.childs) == 2
 
-        sorted_child = deepcopy(node_b.childs).sort(key=lambda node: node.label)
+        sorted_child = deepcopy(node_b.childs)
+        sorted_child.sort(key=lambda node: node.label)
         leaf_c = sorted_child[0]
         assert leaf_c == Node('C', 1)
         leaf_d = sorted_child[0]
@@ -59,7 +60,7 @@ class TestNode():
         root = Node('A')
         root.add_leaf(['A', 'B'], 1)
 
-        assert root.retrieve_leaf_value(['A', 'B']) == [1]
+        assert root.retrieve_leaf_values(['A', 'B']) == [1]
 
     def test_retrieve_leaf_value_depth2(self):
         """Test to retrieve the leaf added in test_add_child_depth2
@@ -67,7 +68,7 @@ class TestNode():
         root = Node('A')
         root.add_leaf(['A', 'B', 'C'], 1)
 
-        assert root.retrieve_leaf_value(['A', 'B', 'C']) == [1]
+        assert root.retrieve_leaf_values(['A', 'B', 'C']) == [1]
 
     def test_retrieve_2_leaf_value_depth2(self):
         """
@@ -78,6 +79,21 @@ class TestNode():
         root.add_leaf(['A', 'B', 'C'], 1)
         root.add_leaf(['A', 'B', 'D'], 2)
 
-        leafs = root.retrieve_leaf_value(['A', 'B'])
+        leafs = root.retrieve_leaf_values(['A', 'B'])
         leafs.sort()
         assert leafs == [1, 2]
+
+    def test_retrieve_1_leaf_value_depth2_2_branch(self):
+        """
+        Test to retrieve the one leafs added in
+        test_add_child_depth2_node_already_exist
+        """
+        root = Node('A')
+        root.add_leaf(['A', 'B', 'C'], 1)
+        root.add_leaf(['A', 'B', 'D'], 2)
+
+        [leaf_c] = root.retrieve_leaf_values(['A', 'B', 'C'])
+        assert leaf_c == 1
+
+        [leaf_d] = root.retrieve_leaf_values(['A', 'B', 'D'])
+        assert leaf_d == 2
