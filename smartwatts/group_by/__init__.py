d324377be7c56a743a0d0bfad04b254db02b3073
Arthur d'Azémar <arthur.d-azemar@inria.fr>
2018-11-08 14:40:33 +0100

    fix import bug

--- smartwatts/group_by/__init__.py
+++ smartwatts/group_by/__init__.py
@@ -1,3 +1,4 @@
+from smartwatts.group_by.abstract_group_by import AbstractGroupBy
 from smartwatts.group_by.test_group_by import TestGroupBy
 from smartwatts.group_by.hwpc_group_by import HWPCGroupBy, HWPCDepthLevel
-from smartwatts.group_by.abstract_group_by import AbstractGroupBy
+
