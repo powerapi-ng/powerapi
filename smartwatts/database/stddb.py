94950f1fa75421b91dd5f0127f2ad79ea5ecb3ad
Jordan Bouchoucha <jordan.bouchoucha@protonmail.com>
2018-11-09 10:09:28 +0100

    stddb -> stdoutdb

--- smartwatts/database/stddb.py
+++
@@ -1,9 +0,0 @@
-from smartwatts.database.base_db import BaseDB
-
-class Stdout(BaseDB):
-    def __init__(self):
-        raise NotImplementedError
-
-class Stdin:
-    def __init__(self):
-        raise NotImplementedError
