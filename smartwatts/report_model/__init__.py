97692d014ed6bae09b0a57921feb6ce6098f8b8f
Jordan Bouchoucha <jordan.bouchoucha@protonmail.com>
2018-11-13 11:20:21 +0100

    report_model + HWPC csv

---
+++ smartwatts/report_model/__init__.py
@@ -0,0 +1,2 @@
+from smartwatts.report_model.report_model import ReportModel, CSV_COMMON
+from smartwatts.report_model.hwpc_model import HWPCModel
---
+++ smartwatts/report_model/hwpc_model.py
@@ -0,0 +1,41 @@
+"""
+Module hwpc_model
+"""
+
+from smartwatts.report_model import ReportModel, CSV_COMMON
+
+
+class HWPCModel(ReportModel):
+    """
+    HWPCModel class.
+    It define all the function for get the hwpc necessary field from
+    any kind of database.
+    """
+
+    def from_mongodb(self):
+        """ Override """
+        pass
+
+    def from_csvdb(self, file_name, row):
+        """ Override """
+        final_dict = {'groups': {}}
+
+        # If group doesn't exist, create it
+        if file_name not in final_dict:
+            final_dict['groups'][file_name] = {}
+
+        # If socket doesn't exist, create it
+        if row['socket'] not in final_dict['groups'][file_name]:
+            final_dict['groups'][file_name][row['socket']] = {}
+
+        # If cpu doesn't exist, create it
+        if row['cpu'] not in final_dict['groups'][file_name][row['socket']]:
+            final_dict['groups'][file_name][row['socket']][row['cpu']] = {}
+
+        # Add events
+        for key, value in row.items():
+            if key not in CSV_COMMON:
+                final_dict['groups'][file_name][
+                    row['socket']][row['cpu']][key] = int(value)
+
+        return final_dict
