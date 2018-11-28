b0706ebf753485ccaa21412d198f933734c87df2
Jordan Bouchoucha <jordan.bouchoucha@protonmail.com>
2018-11-28 16:25:38 +0100

    test: csv test / mongodb js script

--- smartwatts/test/environment/mongodb-js/mongodb_test_mongodb.js
+++ smartwatts/test/environment/mongodb-js/mongodb_test_mongodb.js
@@ -2,6 +2,8 @@
 // !! You don't have to run this script manually, it's done
 // !! in "mongodb.sh" bash script
 
+load('./mongodb-js/report_generator.js');
+
 // Create "test_mongodb" database
 conn = new Mongo();
 db = conn.getDB("test_mongodb");
@@ -17,42 +19,12 @@ db.createCollection("test_mongodb2", {capped: true, size: 256*40});
 // with HWPCReport
 for (var i = 0 ; i < 10 ; i++)
 {
- db.test_mongodb1.insert({
-  _id: i,
-  timestamp: Date.now(),
-  sensor: 'sensor_test',
-  target: 'system',
-  groups: {
-   megagroup: {
-    0 : {
-     0 : {
-      'event1': 100,
-      'event2': 200
-     }
-    }
-   }
-  }
- })
+ db.test_mongodb1.insert(generate_hwpc(i, 'sensor_test', 'system'));
 }
 
 // Feed the collection "test_mongodb2"
 // with HWPCReport
 for (var i = 0 ; i < 10 ; i++)
 {
- db.test_mongodb2.insert({
-  _id: i,
-  timestamp: Date.now(),
-  sensor: 'sensor_test',
-  target: 'system',
-  groups: {
-   megagroup: {
-    0 : {
-     0 : {
-      'event1': 100,
-      'event2': 200
-     }
-    }
-   }
-  }
- })
+ db.test_mongodb2.insert(generate_hwpc(i, 'sensor_test', 'system'));
 }
--- smartwatts/test/environment/mongodb.sh
+++ smartwatts/test/environment/mongodb.sh
@@ -9,6 +9,6 @@ sudo mongod --shutdown --dbpath /data/db-test
 
 # Start mongod
 sudo mongod --port 27017 --fork --logpath /var/log/mongodb.log --dbpath /data/db-test
-mongo mongodb_test_puller.js
-mongo mongodb_test_filter.js
-mongo mongodb_test_mongodb.js
+mongo ./mongodb-js/mongodb_test_puller.js
+mongo ./mongodb-js/mongodb_test_filter.js
+mongo ./mongodb-js/mongodb_test_mongodb.js
