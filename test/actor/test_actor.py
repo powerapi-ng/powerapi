6b49a05ca2e20992e0e2009a0baf45f78feaa570
Arthur d'Azémar <arthur.d-azemar@inria.fr>
2018-12-11 13:55:29 +0100

    test: test that socket are closed after actor termination, test if PAIR socket is correctly initialized

--- test/actor/test_actor.py
+++ test/actor/test_actor.py
@@ -20,6 +20,7 @@ class DummyActor(Actor):
 ACTOR_NAME = 'dummy_actor'
 VERBOSE_MODE = False
 PULL_SOCKET_ADDRESS = 'ipc://@' + ACTOR_NAME
+MONITOR_SOCKET_ADDRESS = 'ipc://@' + 'monitor_' + ACTOR_NAME
 
 
 @pytest.fixture()
@@ -31,6 +32,7 @@ def dummy_actor():
 
 @pytest.fixture()
 def initialized_dummy_actor(dummy_actor):
+    """ return an initialized dummy actor """
     dummy_actor.setup()
     return dummy_actor
 
@@ -39,24 +41,45 @@ def test_actor_initialisation(dummy_actor):
     """ test actor attributes initialization"""
     assert dummy_actor.state.alive is True
     assert dummy_actor.pull_socket_address == PULL_SOCKET_ADDRESS
+    assert dummy_actor.monitor_socket_address == MONITOR_SOCKET_ADDRESS
     assert dummy_actor.name == ACTOR_NAME
 
+
+def _kill_process(dummy_actor):
+    """ check if the kill_process close all sockets
+    """
+    dummy_actor._communication_setup()
+    assert dummy_actor.pull_socket.closed is True
+    assert dummy_actor.monitor_socket.closed is True
+
+    dummy_actor._kill_process()
+
+    assert dummy_actor.pull_socket.closed is False
+    assert dummy_actor.monitor_socket.closed is False
 
 def test_communication_setup(dummy_actor):
     """
     test if zmq context and sockets are correctly initialized and if the proc
     title is correctly set after the run function was call
     """
-
     dummy_actor._communication_setup()
 
-    assert isinstance(dummy_actor.context, zmq.Context)
-    assert isinstance(dummy_actor.pull_socket, zmq.Socket)
-    assert dummy_actor.pull_socket.closed is False
-    assert dummy_actor.pull_socket.get(zmq.TYPE) == zmq.PULL
-    assert (dummy_actor.pull_socket.get(zmq.LAST_ENDPOINT).decode("utf-8") ==
-            PULL_SOCKET_ADDRESS)
     assert setproctitle.getproctitle() == ACTOR_NAME
+
+    assert isinstance(dummy_actor.context, zmq.Context)
+    assert isinstance(dummy_actor.poller, zmq.Poller)
+
+    def check_socket(socket, socket_type, bind_address):
+        assert isinstance(socket, zmq.Socket)
+        assert socket.closed is False
+        assert socket.get(zmq.TYPE) == socket_type
+
+        socket_address = socket.get(zmq.LAST_ENDPOINT).decode("utf-8")
+        assert socket_address == bind_address
+
+    check_socket(dummy_actor.pull_socket, zmq.PULL, PULL_SOCKET_ADDRESS)
+    check_socket(dummy_actor.monitor_socket, zmq.PAIR, MONITOR_SOCKET_ADDRESS)
+
     dummy_actor._kill_process()
 
 
@@ -77,6 +100,7 @@ def test_get_handler(initialized_dummy_actor):
     handler = initialized_dummy_actor.get_corresponding_handler(
         PoisonPillMessage())
     assert isinstance(handler, PoisonPillMessageHandler)
+
 
 def test_behaviour_change(initialized_dummy_actor):
     """ Test if the actor behaviour could be change during the current
