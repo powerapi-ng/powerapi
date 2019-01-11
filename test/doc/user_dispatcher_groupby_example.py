93413789c7e22920cb2ae46283994bcc8edb80f2
Jordan Bouchoucha <jordan.bouchoucha@protonmail.com>
2019-01-11 16:25:25 +0100

    docs: users doc (dispatcher/puller)

---
+++ test/doc/user_dispatcher_groupby_example.py
@@ -0,0 +1,9 @@
+from smartwatts import DispatcherActor
+
+# Create the dispatcher with the formula_factory
+dispatcher = DispatcherActor('dispatcher', formula_factory)
+
+# Create a first rule of group_by with...
+# the report type and the group_by rule
+dispatcher.group_by(HWPCReport, HWPCGroupBy(HWPCDepthLevel.SOCKET,
+                                            primary=True))
