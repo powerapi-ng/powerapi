4e32772c7c139d8d7302ab706a1a180a5bcaa5a2
Guillaume Fieni <gfieni@users.noreply.github.com>
2019-03-11 14:37:50 +0100

    build: Rework of setup script and requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,2 @@
-from setuptools import setup, find_packages
-import powerapi
-
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-setup(name='powerapi',
-      version=powerapi.__version__,
-      description='PowerAPI is a middleware toolkit for building software-defined power meters.',
-      url='http://github.com/powerapi-ng/powerapi',
-      author='Fiene Guillaume, D\'Azémar Arthur, Bouchoucha Jordan, Rouvoy Romain',
-      author_email='powerapi-staff@inria.fr',
-      license='AGPL-3.0',
-      packages=find_packages(exclude=("test",)),
-      install_requires=requirements,
-      zip_safe=False)
+from setuptools import setup
+setup()
