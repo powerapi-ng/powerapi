678c372f10dd83f4dfc58bc573576ff8bf8d9084
Guillaume Fieni <gfieni@users.noreply.github.com>
2019-03-11 14:59:40 +0100

    repo: Relicense project to BSD 3-clause

--- test/unit/utils/test_tree.py
+++ test/unit/utils/test_tree.py
@@ -1,24 +1,37 @@
-# Copyright (C) 2018  University of Lille
-# Copyright (C) 2018  INRIA
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+Copyright (c) 2018, INRIA
+Copyright (c) 2018, University of Lille
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
 
 import pytest
-
 from copy import deepcopy
 from powerapi.utils.tree import Node, Tree
-
 
 
 class TestTree():
@@ -57,6 +70,7 @@ class TestTree():
         assert tree.get([]) == [1]
         assert tree.get(['A']) == [1]
 
+
 class TestNode():
     """
     Test powerapi.tree.Node class
@@ -89,7 +103,6 @@ class TestNode():
         Test to add a leaf to a tree of depth 2 when its parent's node already
         exists
         """
-
         root = Node('A')
         root.add_leaf(['A', 'B', 'C'], 1)
         root.add_leaf(['A', 'B', 'D'], 2)
@@ -107,7 +120,6 @@ class TestNode():
         leaf_d = sorted_child[0]
         assert leaf_d == Node('C', 1)
 
-        
     def test_retrieve_leaf_value_depth1(self):
         """Test to retrieve the leaf added in test_add_child_depth1
         """
@@ -151,7 +163,6 @@ class TestNode():
 
         [leaf_d] = root.retrieve_leaf_values(['A', 'B', 'D'])
         assert leaf_d == 2
-
 
     def test_get_childs_depth1(self):
         """Test to retrieve all childs and their path on test_add_child_depth1
