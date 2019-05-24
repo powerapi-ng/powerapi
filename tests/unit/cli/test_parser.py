20a5bcade2dd5689fffce4742c9976573a5244a5
Arthur d'Azémar <arthur.d-azemar@inria.fr>
2019-05-24 15:47:26 +0200

    feat: add a personalized parser for the cli

---
+++ tests/unit/cli/test_parser.py
@@ -0,0 +1,373 @@
+"""
+Copyright (c) 2018, INRIA
+Copyright (c) 2018, University of Lille
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+
+import pytest
+
+from powerapi.cli.parser import Parser, MainParser, ComponentSubParser
+from powerapi.cli.parser import store_true
+from powerapi.cli.parser import AlreadyAddedArgumentException, BadTypeException
+from powerapi.cli.parser import UnknowArgException, BadContextException, MissingValueException
+
+
+###############
+# PARSER TEST #
+###############
+def test_add_argument_that_aldready_exists():
+    """
+    Add an argument that already exists to a parser and test if an
+    AlreadyAddedArgumentException is raised
+    """
+
+    parser = Parser()
+    parser.add_argument('a')
+
+    with pytest.raises(AlreadyAddedArgumentException):
+        parser.add_argument('a')
+
+
+#####################
+# MAIN PARSER TESTS #
+#####################
+# Test add_argument optargs #
+def test_add_argument_short():
+    """
+    Add a short argument to the parser
+
+    Test if the argument was added to the short_arg string
+    """
+    parser = MainParser()
+    assert parser.short_arg == ''
+    parser.add_argument('a')
+    assert parser.short_arg == 'a:'
+
+
+def test_add_argument_flag():
+    """
+    Add a short flag to the parser
+
+    Test if the argument was added to the short_arg string
+    """
+    parser = MainParser()
+    assert parser.short_arg == ''
+    parser.add_argument('a', flag=True)
+    assert parser.short_arg == 'a'
+
+
+def test_add_argument_2_short():
+    """
+    Add two short argument (an argument and a flag) to the parser
+
+    Test if the arguments was added to the short_arg string
+    """
+    parser = MainParser()
+    assert parser.short_arg == ''
+    parser.add_argument('a', flag=True)
+    assert parser.short_arg == 'a'
+    parser.add_argument('b')
+    assert parser.short_arg == 'ab:'
+
+
+def test_add_argument_long():
+    """
+    Add a long argument to the parser
+
+    Test if the argument was added to the long_arg list
+    """
+    parser = MainParser()
+    assert parser.long_arg == []
+    parser.add_argument('aaa')
+    assert parser.long_arg == ['aaa=']
+
+def test_add_flag_long():
+    """
+    Add a long argument to the parser
+
+    Test if the argument was added to the long_arg list
+    """
+    parser = MainParser()
+    assert parser.long_arg == []
+    parser.add_argument('aaa', flag=True)
+    assert parser.long_arg == ['aaa']
+
+
+
+def check_parsing_result(parser, input_str, outputs):
+
+    result = parser.parse(input_str.split())
+
+    assert len(result) == len(outputs)
+    assert result == outputs
+
+
+def test_empty_parser():
+    """
+    test to parse strings with a parser and retrieve the following results :
+
+    - "" : {}
+    - "-z" : UnknowArgException(z)
+    - "-a" : UnknowArgException(a)
+    - "-a --sub toto -b" : UnknowArgException(a)
+    - "-b" : UnknowArgException(b)
+
+    Parser description :
+
+    - base parser arguments : None
+    - subparser toto binded to the argument sub with sub arguments : None
+    """
+    parser = MainParser()
+
+    check_parsing_result(parser, '', {})
+
+    with pytest.raises(UnknowArgException):
+        check_parsing_result(parser, '-z', None)
+
+    with pytest.raises(UnknowArgException):
+        check_parsing_result(parser, '-a', None)
+
+    with pytest.raises(UnknowArgException):
+        check_parsing_result(parser, '-a --sub toto -b', None)
+
+    with pytest.raises(UnknowArgException):
+        check_parsing_result(parser, '-b', None)
+
+
+def test_main_parser():
+    """
+    test to parse strings with a parser and retrieve the following results :
+
+    - "" : {}
+    - "-z" : UnknowArgException(z)
+    - "-a" : {a: True}
+    - "-a --sub toto -b" : UnknowArgException(sub)
+    - "-b" : UnknowArgException(b)
+
+    Parser description :
+
+    - base parser arguments : -a
+    - subparser toto binded to the argument sub with sub arguments : None
+    """
+    parser = MainParser()
+    parser.add_argument('a', flag=True, action=store_true)
+
+    check_parsing_result(parser, '', {})
+
+    with pytest.raises(UnknowArgException):
+        check_parsing_result(parser, '-z', None)
+
+    check_parsing_result(parser, '-a', {'a': True})
+
+    with pytest.raises(UnknowArgException):
+        check_parsing_result(parser, '-a --sub toto -b', None)
+
+    with pytest.raises(UnknowArgException):
+        check_parsing_result(parser, '-b', None)
+
+
+def test_subparser():
+    """
+    test to parse strings with a parser and retrieve the following results :
+
+    - "" : {}
+    - "-z" : UnknowArgException(z)
+    - "-a" : {a: True}
+    - "-a --sub toto -b" : {a:True, sub: [{type : toto, b: True}]}
+    - "-b" : BadContextException(b, [toto])
+
+    Parser description :
+
+    - base parser arguments : -a
+    - subparser toto binded to the argument sub with sub arguments : -b
+    """
+    parser = MainParser()
+    parser.add_argument('a', flag=True, action=store_true)
+
+    subparser = ComponentSubParser('toto')
+    subparser.add_argument('b', flag=True, action=store_true)
+    parser.add_component_subparser('sub', subparser)
+
+    check_parsing_result(parser, '', {})
+
+    with pytest.raises(UnknowArgException):
+        check_parsing_result(parser, '-z', None)
+
+    check_parsing_result(parser, '-a', {'a': True})
+
+    check_parsing_result(parser, '-a --sub toto -b',
+                         {'a': True, 'sub': [{'type': 'toto', 'b': True}]})
+
+    with pytest.raises(BadContextException):
+        check_parsing_result(parser, '-b', None)
+
+
+def test_argument_with_val():
+    """
+    test to parse strings with a parser and retrieve the following results :
+
+    - "-c" : MissingValue(c)
+    - "-c 1" : {c : 1}
+
+    Parser description :
+
+    - base parser arguments : -c (not flag)
+    """
+    parser = MainParser()
+    parser.add_argument('c')
+
+    with pytest.raises(MissingValueException):
+        check_parsing_result(parser, '-c', None)
+
+    check_parsing_result(parser, '-c 1', {'c': '1'})
+
+
+# Type tests #
+def test_default_type():
+    """
+    add an argument without specifing the type it must catch. Parse a string
+    that contains only this argument and test if the value contained in the
+    result is a string
+
+    """
+    parser = MainParser()
+    parser.add_argument('a')
+    result = parser.parse('-a 1'.split())
+    assert len(result) == 1
+    assert 'a' in result
+    assert isinstance(result['a'], str)
+
+
+def test_other_type():
+    """
+    add an argument that must catch an int value, Parse a string that
+    contains only this argument and test if the value contained in the result is
+    an int
+
+    """
+    parser = MainParser()
+    parser.add_argument('a', type=int)
+    result = parser.parse('-a 1'.split())
+    assert len(result) == 1
+    assert 'a' in result
+    assert isinstance(result['a'], int)
+
+
+def test_cant_convert_to_type():
+    """
+    add an argument that must catch an int value, Parse a string that
+    contains only this argument with a value that is not an int test if an
+    """
+    parser = MainParser()
+    parser.add_argument('a', type=int)
+
+    with pytest.raises(BadTypeException):
+        parser.parse('-a a'.split())
+
+
+# parse with ComponentSubparser tests #
+def test_add_component_subparser_that_aldready_exists():
+    """
+    Add a component_subparser that already exists to a parser and test if an
+    AlreadyAddedArgumentException is raised
+    """
+    parser = MainParser()
+    subparser = ComponentSubParser('titi')
+    parser.add_component_subparser('toto', subparser)
+    subparser2 = ComponentSubParser('titi')
+
+    with pytest.raises(AlreadyAddedArgumentException):
+        parser.add_component_subparser('toto', subparser2)
+
+def test_parse_empty_string_default_value():
+    parser = MainParser()
+    parser.add_argument('a', default=1)
+    result = parser.parse(''.split())
+    assert len(result) == 1
+    assert 'a' in result
+    assert result['a'] == 1
+
+############################
+# COMPONENT_SUBPARSER TEST #
+############################
+@pytest.fixture()
+def component_subparser():
+    """
+    A sub parser with one argument "-a"
+    """
+    subparser = ComponentSubParser('test')
+    subparser.add_argument('a', flag=True)
+    return subparser
+
+
+def test_component_subparser_empty(component_subparser):
+    """
+    test component_subparser, parse an empty token list. 
+
+    must return return an empty dictionary as parse result and a empty token
+    list
+
+    """
+    assert component_subparser.subparse([]) == ([], {})
+
+
+def test_component_subparser_normal_token_list(component_subparser):
+    """
+    test component_subparser, parse a token list which contain only subparser
+    argument [('a', '')].
+
+    must return return a  dictionary {'type': 'test', 'a':''} as parse result
+    and a empty token list
+
+    """
+    assert component_subparser.subparse([('a', '')]) == ([], {'type': 'test',
+                                                              'a': None})
+
+
+def test_component_subparser_full_token_list(component_subparser):
+    """
+    test component_subparser, parse a token list which contain subparser
+    argument and arguments from other parser[('a', ''), ('b', '')].
+
+    must return return a  dictionary {'type': 'test', 'a':''} as parse result
+    and a token list that contains the unparsed arguments : [('b', '')].
+
+    """
+    assert component_subparser.subparse([('a', ''), ('b', '')]) == ([('b', '')],
+                                                                    {'type': 'test', 'a': None})
+
+
+def test_subparser_empty_token_list_default_value():
+    subparser = ComponentSubParser('toto')
+    subparser.add_argument('a', default=1)
+    acc, result = subparser.subparse([])
+    assert len(result) == 1
+    assert 'a' in result
+    assert result['a'] == 1
+    assert acc == []
