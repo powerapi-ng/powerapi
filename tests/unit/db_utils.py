5d116921c524f361dc3de8e94de0aafde1a5c365
Arthur d'Azémar <arthur.d-azemar@inria.fr>
2020-01-30 18:06:59 +0100

    tests: add puller test

--- tests/unit/db_utils.py
+++ tests/unit/db_utils.py
@@ -1,21 +1,21 @@
 # Copyright (c) 2018, INRIA
 # Copyright (c) 2018, University of Lille
 # All rights reserved.
-
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
-
+#
 # * Redistributions in binary form must reproduce the above copyright notice,
 #   this list of conditions and the following disclaimer in the documentation
 #   and/or other materials provided with the distribution.
-
+#
 # * Neither the name of the copyright holder nor the names of its
 #   contributors may be used to endorse or promote products derived from
 #   this software without specific prior written permission.
-
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -27,31 +27,24 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-"""
-Messages unit test
-"""
-import pytest
-
-from powerapi.message import PoisonPillMessage
-
-#####################
-# PoisonPillMessage #
-#####################
-def test_create_PoisonPillMessage_with_soft_True_set_attribute_is_soft_to_True():
-    msg = PoisonPillMessage(soft=True)
-    assert msg.is_soft
+from powerapi.database import BaseDB
+from multiprocessing import Queue
 
 
-def test_create_PoisonPillMessage_with_soft_True_set_attribute_is_hard_to_False():
-    msg = PoisonPillMessage(soft=True)
-    assert not msg.is_hard
+class FakeDB(BaseDB):
 
+    def __init__(self, content=[], *args, **kwargs):
+        self._content = content
+        self.q = Queue()
 
-def test_create_PoisonPillMessage_with_soft_False_set_attribute_is_soft_to_False():
-    msg = PoisonPillMessage(soft=False)
-    assert not msg.is_soft
+    def connect(self):
+        self.q.put('connected', block=False)
 
+    def iter(self, report_model, stream_mode):
+        return self._content.__iter__()
 
-def test_create_PoisonPillMessage_with_soft_False_set_attribute_is_hard_to_True():
-    msg = PoisonPillMessage(soft=False)
-    assert msg.is_hard
+    def save(self, report, report_model):
+        self.q.put(report, block=False)
+
+    def save_many(self, reports, report_model):
+        self.q.put(reports, block=False)
