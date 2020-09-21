781d741e92361a04e0d9bf585dcabba9b552429f
adazemar <arthur.d-azemar@inria.fr>
2020-09-21 15:39:05 +0200

    build: fix ci config

--- build_pypy_image.sh
+++
@@ -1,9 +0,0 @@
-mkdir powerapi_pypy
-cp -R powerapi powerapi_pypy/
-cp setup.cfg setup.py powerapi_pypy/
-cp pypy/to_3.6.sh pypy/Dockerfile powerapi_pypy/
-
-cd powerapi_pypy
-sh to_3.6.sh
-version=$(python3 -c "import powerapi; print(powerapi.__version__)")
-docker build . --tag powerapi/powerapi:$version-pypy
--- pypy/to_3.6.sh
+++
@@ -1,4 +0,0 @@
-sed -i 's/ *-> *[[:alnum:]]*Report//g' $(find powerapi/ -name "*.py")
-sed -i 's/from __future__ import annotations//g' $(find powerapi/ -name "*.py")
-sed -i 's/python_requires = >= 3\.[0-9]/python_requires = >= 3.6/g' setup.cfg
-					 
