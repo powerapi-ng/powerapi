18bb83a1eeaad55419bfc07e50c1444b14078139
adazemar <arthur.d-azemar@inria.fr>
2021-09-10 16:42:27 +0200

    fix setup

--- tests/integration/database/test_direct_prometheus_db.py
+++ tests/integration/database/test_direct_prometheus_db.py
@@ -36,12 +36,11 @@ import pytest
 
 from powerapi.database import DirectPrometheusDB
 from powerapi.report import PowerReport
+from powerapi.test_utils.abstract_test import recv_from_pipe
 
-PORT = 9999
 ADDR = 'localhost'
 METRIC = 'test'
 DESC = 'TEST'
-URL = 'http://' + ADDR + ':' + str(PORT) + '/metrics'
 
 TARGET1 = 'targetA'
 TARGET2 = 'targetB'
@@ -75,80 +74,103 @@ def extract_metrics(metric_prefix, url):
     return metrics
 
 class DirectPrometheusServer(multiprocessing.Process):
-    def __init__(self, q):
+    def __init__(self, q, port):
         multiprocessing.Process.__init__(self)
+        self.port = port
         self.q = q
 
     def run(self):
-        db = DirectPrometheusDB(PowerReport, PORT, ADDR, METRIC, DESC, ['socket'])
+        db = DirectPrometheusDB(PowerReport, self.port, ADDR, METRIC, DESC, ['socket'])
         db.connect()
+        self.q.put('ok')
         while True:
             report_list = self.q.get()
             db.save_many(report_list)
 
+def _gen_serv(unused_tcp_port_factory):
+    port = unused_tcp_port_factory()
+    q = multiprocessing.Queue()
+    p = DirectPrometheusServer(q, port)
+    p.start()
+    return port, q, p
+            
 @pytest.fixture
-def db():
+def db_info(unused_tcp_port_factory):
     """
     start a DirectPrometheusDB in a process and return a q to send report to the DB
     """
-    q = multiprocessing.Queue()
-    p = DirectPrometheusServer(q)
-    p.start()
-    yield q
+    port, q, p = _gen_serv(unused_tcp_port_factory)
+    if q.get(timeout=1) == 'ok':
+        yield q, _gen_url(port)
+    else:
+        p.terminate()
+        port, q, p = _gen_serv(unused_tcp_port_factory)
+        yield q, _gen_url(port)
     p.terminate()
 
+def _gen_url(port):
+    return 'http://' + ADDR + ':' + str(port) + '/metrics'
 
-def test_create_direct_prometheus_db_and_connect_it_must_launch_web_server_on_given_address(db):
-    r = requests.get(URL)
+
+def test_create_direct_prometheus_db_and_connect_it_must_launch_web_server_on_given_address(db_info):
+    db, url = db_info
+    r = requests.get(url)
     assert r.status_code == 200
 
 
-def test_create_direct_prometheus_db_and_dont_connect_it_must_not_launch_web_server_on_given_address():
-    db = DirectPrometheusDB(PowerReport, PORT, ADDR, METRIC, DESC, ['socket'])
+def test_create_direct_prometheus_db_and_dont_connect_it_must_not_launch_web_server_on_given_address(unused_tcp_port):
+    db = DirectPrometheusDB(PowerReport, unused_tcp_port, ADDR, METRIC, DESC, ['socket'])
     with pytest.raises(requests.exceptions.ConnectionError):
-        r = requests.get(URL)
+        r = requests.get(_gen_url(unused_tcp_port))
 
 
-def test_save_one_report_must_expose_data(db):
+def test_save_one_report_must_expose_data(db_info):
+    db, url = db_info
     db.put([REPORTA_1])
-    assert extract_metrics(METRIC, URL) != {}
+    assert extract_metrics(METRIC, url) != {}
 
 
-def test_save_one_report_must_expose_energy_metric_for_the_given_target(db):
+def test_save_one_report_must_expose_energy_metric_for_the_given_target(db_info):
+    db, url = db_info
     db.put([REPORTA_1])
-    data = extract_metrics(METRIC, URL)
+    data = extract_metrics(METRIC, url)
     assert TARGET1 in data
 
-    
-def test_save_one_report_must_expose_correct_value(db):
+
+def test_save_one_report_must_expose_correct_value(db_info):
+    db, url = db_info
     db.put([REPORTA_1])
-    data = extract_metrics(METRIC, URL)
+    data = extract_metrics(METRIC, url)
     assert data[TARGET1][0]['value'] == 10
 
 
-def test_save_two_reports_with_same_target_must_expose_correct_energy_value_for_second_report(db):
+def test_save_two_reports_with_same_target_must_expose_correct_energy_value_for_second_report(db_info):
+    db, url = db_info
     db.put([REPORTA_1, REPORTA_2])
-    data = extract_metrics(METRIC, URL)
+    data = extract_metrics(METRIC, url)
     assert data[TARGET1][0]['value'] == 20
 
 
-def test_save_two_report_with_different_target_must_expose_data_for_the_two_target(db):
+def test_save_two_report_with_different_target_must_expose_data_for_the_two_target(db_info):
+    db, url = db_info
     db.put([REPORTA_1, REPORTB_1])
-    data = extract_metrics(METRIC, URL)
+    data = extract_metrics(METRIC, url)
     assert TARGET1 in data
     assert TARGET2 in data
 
 
-def test_save_two_report_with_different_target_must_expose_correct_data_for_each_target(db):
+def test_save_two_report_with_different_target_must_expose_correct_data_for_each_target(db_info):
+    db, url = db_info
     db.put([REPORTA_1, REPORTB_1])
 
-    data = extract_metrics(METRIC, URL)
+    data = extract_metrics(METRIC, url)
     assert data[TARGET1][0]['value'] == 10
     assert data[TARGET2][0]['value'] == 40
 
 
-def test_save_report_from_two_target_and_then_report_from_one_target_must_finaly_only_expose_report_from_remaining_target(db):
+def test_save_report_from_two_target_and_then_report_from_one_target_must_finaly_only_expose_report_from_remaining_target(db_info):
+    db, url = db_info
     db.put([REPORTA_1, REPORTB_1, REPORTA_2, REPORTA_3])
-    data = extract_metrics(METRIC, URL)
+    data = extract_metrics(METRIC, url)
     assert TARGET1 in data
     assert TARGET2 not in data
