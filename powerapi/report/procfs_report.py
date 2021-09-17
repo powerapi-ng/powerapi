b1bf46949c102801ea8a6362a4763439868cfbab
Lauric Desauw <lauric.desauw@inria.fr>
2021-09-17 11:26:09 +0200

    Format code for linter

--- powerapi/report/procfs_report.py
+++ powerapi/report/procfs_report.py
@@ -90,14 +90,17 @@ class ProcfsReport(Report):
 
     @staticmethod
     def from_mongodb(data: Dict) -> ProcfsReport:
+        """ Extract a PorcfsReport fropm a mongo DB"""
         return ProcfsReport.from_json(data)
 
     @staticmethod
     def to_mongodb(report: ProcfsReport) -> Dict:
+        """ Export a ProcfsReport to a mongo DB"""
         return ProcfsReport.to_json(report)
 
     @staticmethod
     def from_csv_lines(lines: List[Tuple[str, Dict]]) -> ProcfsReport:
+        """ Extract a ProcfsReport from a csv"""
         sensor_name = None
         target = None
         timestamp = None
@@ -109,37 +112,34 @@ class ProcfsReport(Report):
             try:
                 if sensor_name is None:
                     sensor_name = row['sensor']
+                    target = row['target']
+                    timestamp = ProcfsReport._extract_timestamp(row['timestamp'])
+                    global_cpu_usage = row['global_cpu_usage']
                 else:
                     if sensor_name != row['sensor']:
                         raise BadInputData('csv line with different sensor name are mixed into one report', {})
-                if target is None:
-                    target = row['target']
-                else:
                     if target != row['target']:
                         raise BadInputData('csv line with different target are mixed into one report', {})
-                if timestamp is None:
-                    timestamp = ProcfsReport._extract_timestamp(row['timestamp'])
-                else:
                     if timestamp != ProcfsReport._extract_timestamp(row['timestamp']):
                         raise BadInputData('csv line with different timestamp are mixed into one report', {})
-
-                if global_cpu_usage is None:
-                    global_cpu_usage = row['global_cpu_usage']
-                else:
                     if global_cpu_usage != row['global_cpu_usage']:
-                        raise BadInputData('Different cpu usage are provided in one report')
+                        raise BadInputData('Different cpu usage are provided in one report', {})
 
-                if cgroup_name not in usage:
-                    usage[cgroup_name] = {}
-
-                for key, value in row.items():
-                    if key not in CSV_HEADER_PROCFS:
-                        usage[cgroup_name][key] = int(value)
+                ProcfsReport._create_cgroup(row, cgroup_name, usage)
 
             except KeyError as exn:
-                raise BadInputData('missing field ' + str(exn.args[0]) + ' in csv file ' + file_name)
+                raise BadInputData('missing field ' + str(exn.args[0]) + ' in csv file ' + file_name, {}) from exn
 
         return ProcfsReport(timestamp, sensor_name, target, usage, global_cpu_usage)
+
+    @staticmethod
+    def _create_cgroup(row, cgroup_name, usage):
+        if cgroup_name not in usage:
+            usage[cgroup_name] = {}
+
+        for key, value in row.items():
+            if key not in CSV_HEADER_PROCFS:
+                usage[cgroup_name][key] = int(value)
 
 #############################
 # REPORT CREATION FUNCTIONS #
@@ -172,6 +172,7 @@ class ProcfsReport(Report):
 
 
 def create_report_root(cgroup_list, timestamp=datetime.fromtimestamp(0), sensor='toto', target='all'):
+    """ Create a default procfs report """
     sensor = ProcfsReport(timestamp=timestamp, sensor=sensor, target=target, usage={}, global_cpu_usage=0)
     for (cgroup_id, cpu_usage) in cgroup_list:
         sensor.usage[cgroup_id] = cpu_usage
--- powerapi/utils/sync.py
+++ powerapi/utils/sync.py
@@ -28,7 +28,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from queue import Queue
-import datetime
+
 from powerapi.exception import PowerAPIException
 
 
