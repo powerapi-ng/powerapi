8acf85b6e64410f6499339f1177f6b958023a56f
Lauric Desauw <lauric.desauw@inria.fr>
2021-11-16 14:00:10 +0100

    fix: test bug metadata

---
+++ tests/unit/report/report.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2021, INRIA
+# Copyright (c) 2021, University of Lille
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import pytest
+
+from powerapi.report import Report, BadInputData
+from datetime import datetime
+
+############
+# METADATA #
+############
+
+def test_creating_report_with_metadata():
+    report = Report(('1970-09-01T09:09:10.543'), 'toto', 'all', {"tag": 1})
+    assert report.metadata["tag"] == 1
+
+
+def test_create_two_report_without_metadata_metadata_are_different():
+    """
+    When using a default parameter, its value is evaluated at the instantiation of the class
+    So if not used carefully, all object have the same value as attribute
+    """
+
+    a = Report(0,'toto', 'all')
+    a.metadata["test"] = "value"
+    b = Report(0,'toto', 'all')
+    assert a.metadata != b.metadata
