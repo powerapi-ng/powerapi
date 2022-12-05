c0cdee788399e922defaef618c4c72442aa10da5
daniel <daniel.romero-acero@univ-lille.fr>
2022-12-05 11:51:27 +0100

    refactor: remove unnecessary scripts

--- tests/unit/doc/user_dispatcher_groupby_example.py
+++
@@ -1,9 +0,0 @@
-from powerapi import DispatcherActor
-
-# Create the dispatcher with the formula_factory
-dispatcher = DispatcherActor('dispatcher', formula_factory)
-
-# Create a first rule of dispatch_rule with...
-# the report type and the dispatch_rule rule
-dispatcher.dispatch_rule(HWPCReport, HWPCDispatchRule(HWPCDepthLevel.SOCKET,
-                                            primary=True))
