3dc0cf813c6d1a2a5177d8dd91107f95dfb95474
daniel <daniel.romero@inria.fr>
2023-07-18 11:41:12 +0200

    refactor: Remove useless import

--- tests/unit/cli/test_config_parser.py
+++ tests/unit/cli/test_config_parser.py
@@ -27,7 +27,6 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 import pytest
-from samba.dcerpc.preg import file
 
 from powerapi.cli.config_parser import ConfigurationArgument, BaseConfigParser, RootConfigParser, SubgroupConfigParser
 from powerapi.cli.config_parser import store_true, store_val
