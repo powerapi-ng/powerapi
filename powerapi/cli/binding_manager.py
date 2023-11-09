11c02bd67f30e784061b1efff5f236c948c5d1e0
daniel <daniel.romero@inria.fr>
2023-11-09 09:44:51 +0100

    build: Define kuberntes as a group and Add it to all-platforms group

--- powerapi/cli/binding_manager.py
+++ powerapi/cli/binding_manager.py
@@ -26,6 +26,7 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# pylint: disable=R1702
 from powerapi.exception import UnsupportedActorTypeException, UnexistingActorException, TargetActorAlreadyUsed
 from powerapi.processor.processor_actor import ProcessorActor
 from powerapi.puller import PullerActor
