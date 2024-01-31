f50bcee392ea46f9abdb240b7fc9922f2605e4d3
daniel <daniel.romero@inria.fr>
2024-01-31 10:26:51 +0100

    test: Add test for Rest Server

---
+++ tests/integration/database/conftest.py
@@ -0,0 +1,82 @@
+# Copyright (c) 2024, INRIA
+# Copyright (c) 2024, University of Lille
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import pytest
+
+from powerapi.database.http_db import HttpServerDB
+from tests.integration.database.test_http_server import REPORT_TYPE
+from tests.utils.report.hwpc import extract_rapl_reports_with_2_sockets
+
+
+@pytest.fixture()
+def server(host_token_port: tuple) -> HttpServerDB:
+    """
+    Create a server with the given host and REPORT_TYPE as report type and PORT as port
+    :param host_token_port: Tuple (host, token) with the server host and the token to be defined when authentification is
+    enabled
+    """
+    host = host_token_port[0]
+    token = host_token_port[1]
+    port = host_token_port[2]
+
+    the_server = HttpServerDB(report_type=REPORT_TYPE, host=host, port=port, token=token)
+
+    yield the_server
+
+    the_server.stop()
+
+
+@pytest.fixture()
+def started_server(server) -> HttpServerDB:
+    """
+    Create and start a server with the given host and REPORT_TYPE as report type and PORT as port
+    :param server: The server to be started
+    """
+    server.connect()
+
+    yield server
+
+
+@pytest.fixture()
+def rest_client(started_server):
+    """
+    Return a client by using the provided server
+    :param started_server The started server
+    """
+    app = started_server.flask_app
+    with app.test_client() as client:
+        yield client
+
+
+@pytest.fixture()
+def hwpc_reports(number_of_reports_to_extract: int):
+    """
+    Return a list of dicts, each one representing a hwpc report
+    """
+    return extract_rapl_reports_with_2_sockets(number_of_reports=number_of_reports_to_extract)
---
+++ tests/integration/database/test_http_server.py
@@ -0,0 +1,132 @@
+# Copyright (c) 2024, Inria
+# Copyright (c) 2024, University of Lille
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import pytest
+from werkzeug.datastructures import Authorization
+
+from powerapi.database.http_db import HttpServerDB, CREATED_CODE, NOT_FOUND_CODE, UNAUTHORIZED_CODE, HWPC_RESOURCE_PATH
+from powerapi.report import HWPCReport
+
+REPORT_TYPE = HWPCReport
+LOCALHOST = 'localhost'
+WRONG_HOST_1 = '196.54.56.4'
+WRONG_HOST_2 = '0.0.0.1'
+
+ALL_PUBLIC_IPS = '0.0.0.0'
+
+PORT = 10000
+
+NUMBER_OF_REPORTS_FOR_TESTING = 50
+
+
+@pytest.mark.parametrize('host_token_port', [(LOCALHOST, None, PORT), (ALL_PUBLIC_IPS, None, PORT)])
+def test_correct_connection(server):
+    """
+    Start the http server and test that no exception is raised
+    """
+    server.connect()
+    assert True
+
+
+@pytest.mark.parametrize('host', [WRONG_HOST_1, WRONG_HOST_2])
+def test_server_init_raise_error_with_wrong_host(host):
+    """
+    Test if an exception is raised when creating a server with a wrong host
+    """
+    with pytest.raises(OSError):
+        _ = HttpServerDB(report_type=REPORT_TYPE, host=host, port=PORT)
+
+
+@pytest.mark.parametrize('number_of_reports_to_extract, host_token_port',
+                         [(NUMBER_OF_REPORTS_FOR_TESTING, (LOCALHOST, None, PORT))])
+def test_process_hwpc_report_without_authentification_works_with_correct_uri(hwpc_reports, started_server, rest_client):
+    """
+    Test that the processing with authentification disabled (token = None) is working when using a correct uri
+    """
+    for report_number in range(0, NUMBER_OF_REPORTS_FOR_TESTING):
+        response = rest_client.post(HWPC_RESOURCE_PATH, json=hwpc_reports[report_number])
+        assert response.status_code == CREATED_CODE
+
+    assert started_server.report_queue.qsize() == NUMBER_OF_REPORTS_FOR_TESTING
+
+
+@pytest.mark.parametrize('number_of_reports_to_extract, host_token_port',
+                         [(NUMBER_OF_REPORTS_FOR_TESTING, (LOCALHOST, None, PORT))])
+def test_process_hwpc_report_without_authentification_fails_with_wrong_resource_uri(hwpc_reports,
+                                                                                    started_server, rest_client):
+    """
+    Test that the processing with authentification disabled (token = None) fails when a wrong uri is used
+    """
+    for report_number in range(0, NUMBER_OF_REPORTS_FOR_TESTING):
+        response = rest_client.post('/api/v1/report/PowerReport', json=hwpc_reports[report_number])
+        assert response.status_code == NOT_FOUND_CODE
+
+    assert started_server.report_queue.qsize() == 0
+
+
+@pytest.mark.parametrize('number_of_reports_to_extract, host_token_port',
+                         [(NUMBER_OF_REPORTS_FOR_TESTING, (LOCALHOST, 'my_token', PORT + 1))])
+def test_process_hwpc_report_with_authentification_works_with_login(host_token_port, hwpc_reports, started_server,
+                                                                    rest_client):
+    """
+    Test that the processing with authentification enable (token != None) is working when using a correct token
+    """
+    for report_number in range(0, NUMBER_OF_REPORTS_FOR_TESTING):
+        response = rest_client.post(HWPC_RESOURCE_PATH, json=hwpc_reports[report_number],
+                                    auth=Authorization(token=host_token_port[1], auth_type='Bearer'))
+        assert response.status_code == CREATED_CODE
+
+    assert started_server.report_queue.qsize() == NUMBER_OF_REPORTS_FOR_TESTING
+
+
+@pytest.mark.parametrize('number_of_reports_to_extract, host_token_port',
+                         [(NUMBER_OF_REPORTS_FOR_TESTING, (LOCALHOST, 'my_token', PORT + 2))])
+def test_process_hwpc_report_with_authentification_fails_without_login(hwpc_reports, started_server, rest_client):
+    """
+    Test that the processing with authentification enable (token != None) fails when no token is provided
+    """
+    for report_number in range(0, NUMBER_OF_REPORTS_FOR_TESTING):
+        response = rest_client.post(HWPC_RESOURCE_PATH, json=hwpc_reports[report_number])
+        assert response.status_code == UNAUTHORIZED_CODE
+
+    assert started_server.report_queue.qsize() == 0
+
+
+@pytest.mark.parametrize('number_of_reports_to_extract, host_token_port',
+                         [(NUMBER_OF_REPORTS_FOR_TESTING, (LOCALHOST, 'my_token', PORT + 2))])
+def test_process_hwpc_report_with_authentification_fails_with_wrong_login(hwpc_reports, started_server, rest_client):
+    """
+    Test that the processing with authentification enable (token != None) fails when using a wrong token
+    """
+    for report_number in range(0, NUMBER_OF_REPORTS_FOR_TESTING):
+        response = rest_client.post(HWPC_RESOURCE_PATH, json=hwpc_reports[report_number],
+                                    auth=Authorization(token='wrong_token', auth_type='Bearer'))
+        assert response.status_code == UNAUTHORIZED_CODE
+
+    assert started_server.report_queue.qsize() == 0
