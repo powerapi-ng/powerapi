ef8a370214a0c7067a93bd55e18046e9ab4a0a12
Guillaume Fieni <guillaume.fieni@inria.fr>
2024-02-27 11:42:30 +0100

    test(unit/pusher): Use actor_with_db fixture for tests

--- tests/unit/pusher/test_pusher_actor.py
+++ tests/unit/pusher/test_pusher_actor.py
@@ -33,11 +33,10 @@ from queue import Empty
 
 import pytest
 
-from powerapi.report import Report
 from powerapi.pusher import PusherActor
-from tests.utils.db.db import REPORT2, REPORT1, FakeDB
-
+from powerapi.report import Report
 from tests.unit.actor.abstract_test_actor import AbstractTestActorWithDB, pytest_generate_tests_abstract
+from tests.utils.db.db import REPORT2, REPORT1
 
 
 def define_buffer_size(size):
@@ -98,77 +97,75 @@ class TestPusher(AbstractTestActorWithDB):
     """
 
     @pytest.fixture
-    def actor(self):
-        delay = getattr(self, '_delay', 100)
-        buffer_size = getattr(self, '_buffer_size', 50)
-        return PusherActor('pusher_test', Report, FakeDB(), logging.DEBUG, delay=delay, max_size=buffer_size)
+    def actor_with_db(self, fake_db, delay=100, buffer_size=50):
+        return PusherActor('pusher_test', Report, fake_db, logging.DEBUG, delay=delay, max_size=buffer_size)
 
     @staticmethod
     @define_buffer_size(0)
-    def test_send_one_report_to_pusher_with_0_sized_buffer_make_it_save_the_report(started_actor, fake_db):
+    def test_send_one_report_to_pusher_with_0_sized_buffer_make_it_save_the_report(started_actor_with_db, fake_db):
         """
         Check that the pusher actor saves a report when the buffer size is 0.
         """
-        started_actor.send_data(REPORT1)
+        started_actor_with_db.send_data(REPORT1)
         assert fake_db.q.get(timeout=1) == [REPORT1]
 
     @staticmethod
     @define_buffer_size(1)
-    def test_send_one_report_to_pusher_with_1_sized_buffer_make_it_not_save_the_report(started_actor, fake_db):
+    def test_send_one_report_to_pusher_with_1_sized_buffer_make_it_not_save_the_report(started_actor_with_db, fake_db):
         """
         Check that the pusher actor does not save a report when the buffer size is 1.
         """
-        started_actor.send_data(REPORT1)
+        started_actor_with_db.send_data(REPORT1)
         with pytest.raises(Empty):
             fake_db.q.get(timeout=1)
 
     @staticmethod
     @define_buffer_size(1)
-    def test_send_two_report_to_pusher_with_1_sized_buffer_make_it_save_the_reports_in_one_call(started_actor, fake_db):
+    def test_send_two_report_to_pusher_with_1_sized_buffer_make_it_save_the_reports_in_one_call(started_actor_with_db, fake_db):
         """
         Check that the pusher actor saves the 2 reports when the buffer size is 1.
         """
-        started_actor.send_data(REPORT1)
-        started_actor.send_data(REPORT2)
+        started_actor_with_db.send_data(REPORT1)
+        started_actor_with_db.send_data(REPORT2)
         assert fake_db.q.get(timeout=1) == [REPORT1, REPORT2]
 
     @staticmethod
     @define_delay(0)
-    def test_send_one_report_to_pusher_with_0_delay_make_it_save_the_reports(started_actor, fake_db):
+    def test_send_one_report_to_pusher_with_0_delay_make_it_save_the_reports(started_actor_with_db, fake_db):
         """
         Check that the pusher actor saves a report when the delay is 0.
         """
-        started_actor.send_data(REPORT1)
+        started_actor_with_db.send_data(REPORT1)
         assert fake_db.q.get(timeout=1) == [REPORT1]
 
     @staticmethod
     @define_delay(2000)
-    def test_send_two_report_to_pusher_with_2_seconds_delay_make_it_not_save_the_reports(started_actor, fake_db):
+    def test_send_two_report_to_pusher_with_2_seconds_delay_make_it_not_save_the_reports(started_actor_with_db, fake_db):
         """
         Check that the pusher actor does not save the 2 reports when delay is 2 seconds.
         """
-        started_actor.send_data(REPORT1)
-        started_actor.send_data(REPORT2)
+        started_actor_with_db.send_data(REPORT1)
+        started_actor_with_db.send_data(REPORT2)
         with pytest.raises(Empty):
             fake_db.q.get(timeout=1)
 
     @staticmethod
     @define_delay(2000)
-    def test_send_two_report__with_two_second_between_messages_to_pusher_with_2_seconds_delay_make_it_save_the_report(started_actor, fake_db):
+    def test_send_two_report__with_two_second_between_messages_to_pusher_with_2_seconds_delay_make_it_save_the_report(started_actor_with_db, fake_db):
         """
         Check that the pusher actor saves the 2 reports when delay is 2 seconds and there is a 2 second sleep time between the 2 reports.
         """
-        started_actor.send_data(REPORT1)
+        started_actor_with_db.send_data(REPORT1)
         time.sleep(2)
-        started_actor.send_data(REPORT2)
+        started_actor_with_db.send_data(REPORT2)
         assert fake_db.q.get(timeout=1) == [REPORT1, REPORT2]
 
     @staticmethod
     @define_buffer_size(1)
-    def test_send_two_report_in_wrong_time_order_to_a_pusher_make_it_save_them_in_good_order(started_actor, fake_db):
+    def test_send_two_report_in_wrong_time_order_to_a_pusher_make_it_save_them_in_good_order(started_actor_with_db, fake_db):
         """
         Check that the pusher actor saves the 2 reports in the correct order even if they are not sent in the correct order.
         """
-        started_actor.send_data(REPORT2)
-        started_actor.send_data(REPORT1)
+        started_actor_with_db.send_data(REPORT2)
+        started_actor_with_db.send_data(REPORT1)
         assert fake_db.q.get(timeout=1) == [REPORT1, REPORT2]
