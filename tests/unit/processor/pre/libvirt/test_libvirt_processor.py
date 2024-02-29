e1dc8d610da1be1f22908bb5fcfd51da44ee0d5a
Guillaume Fieni <guillaume.fieni@inria.fr>
2024-02-29 12:41:21 +0100

    test(unit/processor/libvirt): Remove pylint pragma and clean code

--- tests/unit/processor/pre/libvirt/test_libvirt_processor.py
+++ tests/unit/processor/pre/libvirt/test_libvirt_processor.py
@@ -1,21 +1,21 @@
-# Copyright (c) 2023, INRIA
-# Copyright (c) 2023, University of Lille
+# Copyright (c) 2021, INRIA
+# Copyright (c) 2021, University of Lille
 # All rights reserved.
-
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
-
+#
 # * Redistributions in binary form must reproduce the above copyright notice,
 #   this list of conditions and the following disclaimer in the documentation
 #   and/or other materials provided with the distribution.
-
+#
 # * Neither the name of the copyright holder nor the names of its
 #   contributors may be used to endorse or promote products derived from
 #   this software without specific prior written permission.
-
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -27,8 +27,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# pylint: disable=no-self-use,arguments-differ,unused-argument
-
+from datetime import datetime, timezone
 from time import sleep
 import pytest
 from mock.mock import patch
@@ -49,12 +48,14 @@ class TestLibvirtProcessor(AbstractTestActor):
     """
     Class to test the processor related to libvirt
     """
+
+    @staticmethod
     @pytest.fixture
-    def started_fake_dispatcher(self, dummy_pipe_in):
+    def started_fake_dispatcher(dummy_pipe_in):
         """
         Return a started DummyActor. When the test is finished, the actor is stopped
         """
-        dispatcher = DummyActor(name=DISPATCHER_NAME, pipe=dummy_pipe_in, message_type=REPORT_TYPE_TO_BE_SENT)
+        dispatcher = DummyActor(DISPATCHER_NAME, dummy_pipe_in, REPORT_TYPE_TO_BE_SENT)
         dispatcher.start()
 
         yield dispatcher
@@ -62,42 +63,40 @@ class TestLibvirtProcessor(AbstractTestActor):
             dispatcher.terminate()
 
     @pytest.fixture
-    def actor(self, started_fake_dispatcher):
-        with patch('powerapi.processor.pre.libvirt.libvirt_pre_processor_actor.openReadOnly',
-                   return_value=MockedLibvirt()):
-            return LibvirtPreProcessorActor(name='processor_actor', uri='', regexp=REGEXP,
-                                            target_actors=[started_fake_dispatcher])
+    def actor(self, request):
+        with patch('powerapi.processor.pre.libvirt.libvirt_pre_processor_actor.openReadOnly', return_value=MockedLibvirt()):
+            fx_started_fake_dispatcher = request.getfixturevalue('started_fake_dispatcher')
+            return LibvirtPreProcessorActor('processor_actor', '', REGEXP, [fx_started_fake_dispatcher])
 
+    @staticmethod
     @pytest.mark.skip(reason='libvirt is disable by default')
-    def test_modify_report_that_not_match_regexp_must_not_modify_report(self, started_actor,
-                                                                        dummy_pipe_out,
-                                                                        shutdown_system):
+    def test_modify_report_that_not_match_regexp_must_not_modify_report(started_actor, dummy_pipe_out):
         """
         Test that te LibvirtProcessorActor does not modify an report that does not match the regexp
         """
-        report = Report(0, 'sensor', BAD_TARGET)
+        report = Report(datetime.fromtimestamp(0, timezone.utc), 'sensor', BAD_TARGET)
         started_actor.send_data(msg=report)
         sleep(1)
         assert recv_from_pipe(dummy_pipe_out, 2) == (DISPATCHER_NAME, report)
 
+    @staticmethod
     @pytest.mark.skip(reason='libvirt is disable by default')
-    def test_modify_report_that_match_regexp_must_modify_report(self, started_actor, dummy_pipe_out, shutdown_system):
+    def test_modify_report_that_match_regexp_must_modify_report(started_actor, dummy_pipe_out):
         """
         Test that a report matching the regexp of the processor is actually modified
         """
-        report = Report(0, 'sensor', LIBVIRT_TARGET_NAME1)
+        report = Report(datetime.fromtimestamp(0, timezone.utc), 'sensor', LIBVIRT_TARGET_NAME1)
         started_actor.send_data(msg=report)
         new_report = recv_from_pipe(dummy_pipe_out, 2)[1]
         assert new_report.metadata["domain_id"] == UUID_1
 
+    @staticmethod
     @pytest.mark.skip(reason='libvirt is disable by default')
-    def test_modify_report_that_match_regexp_but_with_wrong_domain_name_must_not_modify_report(self, started_actor,
-                                                                                               dummy_pipe_out,
-                                                                                               shutdown_system):
+    def test_modify_report_that_match_regexp_but_with_wrong_domain_name_must_not_modify_report(started_actor, dummy_pipe_out):
         """
         Test that a report matching the regexp but with wrong domain name is not modified by the processor
         """
-        report = Report(0, 'sensor', LIBVIRT_TARGET_NAME2)
+        report = Report(datetime.fromtimestamp(0, timezone.utc), 'sensor', LIBVIRT_TARGET_NAME2)
         started_actor.send_data(msg=report)
         sleep(1)
         assert recv_from_pipe(dummy_pipe_out, 2) == (DISPATCHER_NAME, report)
