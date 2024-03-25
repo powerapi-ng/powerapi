53587879f9035af6dd2c726297467d5d589ca8ab
Guillaume Fieni <guillaume.fieni@inria.fr>
2024-03-25 20:18:00 +0100

    refactor(cli): Remove unused timeout parameters for k8s pre-processor

--- src/powerapi/cli/common_cli_parsing_manager.py
+++ src/powerapi/cli/common_cli_parsing_manager.py
@@ -408,18 +408,6 @@ class CommonCLIParsingManager(RootConfigParsingManager):
         subparser_k8s_pre_processor.add_argument(
             "a", "k8s-api-mode", help_text="k8s api mode (local, manual or cluster)"
         )
-        subparser_k8s_pre_processor.add_argument(
-            "t",
-            "time-interval",
-            help_text="time interval for the k8s monitoring",
-            argument_type=int
-        )
-        subparser_k8s_pre_processor.add_argument(
-            "o",
-            "timeout-query",
-            help_text="timeout for k8s queries",
-            argument_type=int
-        )
 
         subparser_k8s_pre_processor.add_argument(
             "k",
