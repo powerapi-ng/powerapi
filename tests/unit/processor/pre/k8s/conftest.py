4b422d3d51218581b89386da790f8fb7219a9506
Guillaume Fieni <guillaume.fieni@inria.fr>
2024-03-27 18:45:52 +0100

    test(unit/processor/k8s): Rename actor tests file

--- tests/unit/processor/pre/k8s/conftest.py
+++ tests/unit/processor/pre/k8s/conftest.py
@@ -52,5 +52,4 @@ def initialized_monitor_agent(initialized_metadata_cache_manager):
     Returns an initialized monitor agent.
     """
     agent = K8sMonitorAgent(initialized_metadata_cache_manager, 'manual', '', '')
-    agent.k8s_api
     yield agent
