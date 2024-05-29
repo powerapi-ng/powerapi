78c339754a89647e3a29e35412ba04247f20d4cb
Guillaume Fieni <guillaume.fieni@inria.fr>
2024-05-29 18:40:23 +0200

    feat(database/socket): Rework socket database code

--- src/powerapi/database/socket_db.py
+++ src/powerapi/database/socket_db.py
@@ -27,99 +27,151 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import asyncio
-from typing import Type, List
-import json
+import logging
+from json import JSONDecoder, JSONDecodeError
+from queue import SimpleQueue, Empty
+from socketserver import ThreadingMixIn, TCPServer, StreamRequestHandler
+from threading import Thread
+from typing import Type, Iterator
 
-from powerapi.utils import JsonStream
+from powerapi.database.base_db import IterDB, BaseDB
 from powerapi.report import Report
-from .base_db import IterDB, BaseDB, DBError
+
+
+class ThreadedTCPServer(ThreadingMixIn, TCPServer):
+    """
+    TCP Server implementation.
+    Each client connected will be served by a separate thread.
+    """
+    daemon_threads = True
+    allow_reuse_address = True
+
+    def __init__(self, server_address, request_handler_class, received_data_queue: SimpleQueue):
+        """
+        :param server_address: The address to listen on
+        :param request_handler_class: The request handler class to use when receiving requests
+        :param received_data_queue: The data queue to store the received data
+        """
+        super().__init__(server_address, request_handler_class)
+        self.received_data_queue = received_data_queue
+
+
+class JsonRequestHandler(StreamRequestHandler):
+    """
+    Request handler that handles JSON documents received from the client.
+    """
+    server: ThreadedTCPServer
+
+    @staticmethod
+    def parse_json_documents(data: str) -> Iterator[dict]:
+        """
+        Try to parse json document(s) from the given string.
+        This function tolerates truncated and malformed json documents.
+        :param data: The raw data to decode
+        :return: Iterator over parsed json documents
+        """
+        decoder = JSONDecoder()
+        idx = 0
+        while idx < len(data):
+            try:
+                obj, end_idx = decoder.raw_decode(data, idx)
+                yield obj
+                idx += end_idx
+
+            # Search and try to parse the remaining document(s)
+            except JSONDecodeError as e:
+                idx = data.find('{', e.pos)
+                if idx == -1:
+                    break
+
+    def handle(self):
+        """
+        Handle incoming connections.
+        The received data is parsed and the result(s) stored in the data queue for further processing.
+        It is expected for the data to be in json format (utf-8 charset) and newline terminated.
+        """
+        caddr = '{}:{}'.format(*self.client_address)
+        logging.info('New incoming connection from %s', caddr)
+
+        while True:
+            try:
+                data = self.rfile.readline()
+                if not data:
+                    break
+
+                for obj in self.parse_json_documents(data.decode('utf-8')):
+                    self.server.received_data_queue.put(obj)
+
+            except ValueError as e:
+                logging.warning('[%s] Received malformed data: %s', caddr, e)
+                continue
+            except OSError as e:
+                logging.error('[%s] Caught OSError while handling request: %s', caddr, e)
+                break
+            except KeyboardInterrupt:
+                break
+
+        logging.info('Connection from %s closed', caddr)
 
 
 class IterSocketDB(IterDB):
     """
-    iterator connected to a socket that receive report from a sensor
+    SocketDB iterator that returns the received data.
     """
 
-    def __init__(self, report_type, stream_mode, queue):
-        """
-        """
-        IterDB.__init__(self, None, report_type, stream_mode)
-
-        self.queue = queue
-
-    def __aiter__(self):
+    def __iter__(self):
         return self
 
-    async def __anext__(self):
+    def __next__(self):
         try:
-            json_str = await asyncio.wait_for(self.queue.get(), 2)
-            # json = self.queue.get_nowait()
-            # self.queue.get()
-            report = self.report_type.from_json(json.loads(json_str))
-            return report
-        # except Empty:
-        except asyncio.TimeoutError:
-            return None
+            document = self.db.received_data_queue.get(block=False)
+            return self.report_type.from_json(document)
+        except Empty as e:
+            raise StopIteration from e
 
 
 class SocketDB(BaseDB):
     """
-    Database that act as a server that expose a socket where data source will push data
+    Database implementation that exposes a TCP socket the clients can connect to.
     """
 
     def __init__(self, report_type: Type[Report], host: str, port: int):
-        BaseDB.__init__(self, report_type, is_async=True)
-        self.queue = None
-        self.host = host
-        self.port = port
-        self.server = None
+        """
+        :param report_type: The type of report to create
+        :param host: The host address to listen on
+        :param port: The port number to listen on
+        """
+        super().__init__(report_type)
 
-    async def connect(self):
+        self.server_address = (host, port)
+
+        self.received_data_queue = None
+        self.background_thread = None
+
+    def _tcpserver_background_thread_target(self):
+        """
+        Target function of the thread that will run the TCP server in background.
+        """
+        with ThreadedTCPServer(self.server_address, JsonRequestHandler, self.received_data_queue) as server:
+            logging.info('TCP socket is listening on %s:%s', *self.server_address)
+            server.serve_forever()
+
+    def connect(self):
         """
         Connect to the socket database.
         """
-        self.queue = asyncio.Queue()
-        self.server = await asyncio.start_server(self._gen_server_callback(), host=self.host, port=self.port)
+        self.received_data_queue = SimpleQueue()
+        self.background_thread = Thread(target=self._tcpserver_background_thread_target, daemon=True)
+        self.background_thread.start()
 
-    async def disconnect(self):
+    def disconnect(self):
         """
         Disconnect from the socket database.
         """
 
-    async def stop(self):
-        """
-        stop server connection
-        """
-        self.server.close()
-        await self.server.wait_closed()
-
     def iter(self, stream_mode: bool = False) -> IterSocketDB:
-        return IterSocketDB(self.report_type, stream_mode, self.queue)
-
-    def _gen_server_callback(self):
-        async def callback(stream_reader, _):
-            stream = JsonStream(stream_reader)
-            count = 0  # If 10 times in a row we don't have a full message we stop
-            while True:
-                json_str = await stream.read_json_object()
-                if json_str is None:
-                    if count > 10:
-                        break
-                    count += 1
-                    continue
-                count = 0
-                await self.queue.put(json_str)
-
-                # self.queue.put(json_str)
-
-        return callback
-
-    def __iter__(self):
-        raise DBError('Socket db don\'t support __iter__ method')
-
-    def save(self, report: Report):
-        raise DBError('Socket db don\'t support save method')
-
-    def save_many(self, reports: List[Report]):
-        raise DBError('Socket db don\'t support save_many method')
+        """
+        Create the data iterator for the socket database.
+        :param stream_mode: Whether the data should be pulled continuously or not.
+        """
+        return IterSocketDB(self, self.report_type, stream_mode)
