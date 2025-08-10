17042ac4f41f97d7992230ae69cfd78654a2c6a6
Guillaume Fieni <guillaume.fieni@inria.fr>
2025-08-10 16:11:43 +0200

    test(unit/pusher): Cleanup actor tests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -26,9 +26,13 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 from multiprocessing import active_children
 
 import pytest
+
+from powerapi.report import Report
+from tests.utils.db import SilentFakeDB
 
 
 @pytest.fixture
@@ -43,245 +47,19 @@ def shutdown_system():
 
 
 @pytest.fixture
-def power_timeline():
+def make_fake_database():
     """
-    Timeline of procfs report for the tests
+    Factory fixture for creating a fake database with optional content.
     """
+    def _make(content: list[Report] | None = None) -> SilentFakeDB:
+        return SilentFakeDB(content or [])
 
-    return [
-        {
-            "timestamp": "2021-09-14T12:37:37.168817",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:37.669237",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:38.170142",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:38.670338",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:39.171321",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:39.671572",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:40.172503",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:40.672693",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:41.173552",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:41.673815",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:42.174560",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:42.674690",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:43.175441",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:43.675743",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:44.176551",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:44.677307",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:45.178049",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:45.678310",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:46.179120",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:46.679308",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:47.180223",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:47.680468",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:48.181316",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:48.681683",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:49.182522",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:49.682731",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:50.183680",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:50.683812",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:51.184792",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:51.685027",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:52.185709",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:52.686065",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:53.186929",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:53.687190",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:54.188031",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:54.688674",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:55.189489",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:55.690299",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
-        {
-            "timestamp": "2021-09-14T12:37:56.191124",
-            "sensor": "formula_group",
-            "target": "all",
-            "power": 42,
-        },
+    return _make
 
-    ]
+
+@pytest.fixture
+def fake_database(request) -> SilentFakeDB:
+    """
+    Fixture to create a SilentFakeDB without content.
+    """
+    return SilentFakeDB()
--- tests/unit/pusher/conftest.py
+++ tests/unit/pusher/conftest.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2021, Inria
-# Copyright (c) 2021, University of Lille
+# Copyright (c) 2025, Inria
+# Copyright (c) 2025, University of Lille
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -27,4 +27,22 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from .db import SilentFakeDB, make_report, generate_reports
+import pytest
+
+from powerapi.actor import Actor
+from powerapi.pusher.handlers import ReportHandler
+from powerapi.pusher.pusher_actor import PusherState
+
+
+@pytest.fixture
+def pusher_report_handler(fake_database):
+    """
+    Factory fixture for creating a pusher report handler.
+    """
+    def _create_handler(flush_interval: float = 1.0, buffer_size: int = 10, last_write_ts: float = 0.0):
+        state = PusherState(Actor('test-actor'), fake_database)
+        handler = ReportHandler(state, flush_interval, buffer_size)
+        handler._last_write_ts = last_write_ts
+        return handler
+
+    return _create_handler
