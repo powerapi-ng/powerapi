528192200b943f9f070ac0c7c43458da39171442
Guillaume Fieni <guillaume.fieni@inria.fr>
2025-10-28 17:33:20 +0100

    refactor(database/prometheus): Remove unused `ABC` import

--- src/powerapi/database/prometheus/collectors.py
+++ src/powerapi/database/prometheus/collectors.py
@@ -27,7 +27,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from collections.abc import Iterable
 from threading import Lock
 from time import time
