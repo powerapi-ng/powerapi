0064a3008ee04132e138f1f335a68e4548f277ad
Guillaume Fieni <guillaume.fieni@inria.fr>
2025-11-03 19:53:36 +0100

    refactor(handler): Guard type hints imports with `TYPE_CHECKING`

--- src/powerapi/handler/handler.py
+++ src/powerapi/handler/handler.py
@@ -27,8 +27,12 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from typing import TYPE_CHECKING
+
 from powerapi.exception import PowerAPIException, UnknownMessageTypeException
-from powerapi.actor.message import Message
+
+if TYPE_CHECKING:
+    from powerapi.actor.message import Message
 
 
 class HandlerException(PowerAPIException):
