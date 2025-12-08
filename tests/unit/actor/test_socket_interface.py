68e32a82eb8b2ac98e5faec91ff19ded14873660
Guillaume Fieni <guillaume.fieni@inria.fr>
2025-12-08 16:19:30 +0100

    feat(actor): Rework socket interface with ZeroMQ IPC sockets

--- tests/unit/actor/test_socket_interface.py
+++ tests/unit/actor/test_socket_interface.py
@@ -27,43 +27,43 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import secrets
+from pathlib import Path
+
 import pytest
 import zmq
 
-from powerapi.actor import SocketInterface
-
-ACTOR_NAME = 'dummy_actor'
-PULL_SOCKET_ADDRESS = 'ipc://@' + ACTOR_NAME
-CONTROL_SOCKET_ADDRESS = 'ipc://@' + 'control_' + ACTOR_NAME
+from powerapi.actor import SocketInterface, NotConnectedException
 
 
-def check_socket(socket, socket_type, bind_address):
-    """check if the given socket is open, binded to the correct address and have
-    the correct type
-
+def check_socket(socket: zmq.Socket, socket_type: int, socket_filepath: Path) -> None:
+    """
+    Checks if the given socket is open, bound to the correct address and have the correct type.
+    :param socket: Socket instance to check
+    :param socket_type: Expected socket type
+    :param socket_filepath: Expected filepath of the IPC socket
     """
     assert isinstance(socket, zmq.Socket)
     assert socket.closed is False
     assert socket.get(zmq.TYPE) == socket_type
 
-    socket_address = socket.get(zmq.LAST_ENDPOINT).decode("utf-8")
-    assert socket_address == bind_address
+    socket_address = socket.getsockopt_string(zmq.LAST_ENDPOINT)
+    assert socket_address == f'ipc://{socket_filepath}'
 
 
 @pytest.fixture
 def socket_interface():
-    """Return a socket interface not initialized
-
     """
-    return SocketInterface(ACTOR_NAME, 100)
+    Returns an uninitialized socket interface.
+    """
+    actor_name = f'pytest-{secrets.token_hex()}'  # Prevent collisions on IPC files between runs.
+    return SocketInterface(actor_name, 100)
 
 
 @pytest.fixture
-def initialized_socket_interface(socket_interface):
-    """Return an initialized socket interface
-
-    close the socket interface after testing
-
+def endpoint_interface(socket_interface):
+    """
+    Returns a socket interface acting as endpoint.
     """
     socket_interface.setup()
     yield socket_interface
@@ -71,122 +71,177 @@ def initialized_socket_interface(socket_interface):
 
 
 @pytest.fixture
-def connected_interface(initialized_socket_interface):
-    """ Return an initialized socket interface with an open connection to the
-    push socket
-
+def data_peer_interface(socket_interface):
     """
-    initialized_socket_interface.connect_data()
-    yield initialized_socket_interface
-    initialized_socket_interface.close()
+    Returns a socket interface connected to the data socket.
+    """
+    socket_interface.connect_data()
+    yield socket_interface
+    socket_interface.close()
 
 
 @pytest.fixture
-def controlled_interface(initialized_socket_interface):
-    """ Return an initialized socket interface with an open connection to the
-    control socket
-
+def control_peer_interface(socket_interface):
     """
-    initialized_socket_interface.connect_control()
-    yield initialized_socket_interface
-    initialized_socket_interface.close()
+    Returns a socket interface connected to the control socket.
+    """
+    socket_interface.connect_control()
+    yield socket_interface
+    socket_interface.close()
 
 
 @pytest.fixture
-def fully_connected_interface(initialized_socket_interface):
-    """ Return an initialized socket interface with an open connection to the
-    control and the push socket
-
+def peer_interface(socket_interface):
     """
-    initialized_socket_interface.connect_data()
-    initialized_socket_interface.connect_control()
-    yield initialized_socket_interface
-    initialized_socket_interface.close()
-
-
-def test_close(initialized_socket_interface):
-    """ test if the close method close the control and pull socket
+    Returns a socket interface connected to both control and data sockets.
     """
-    assert initialized_socket_interface.pull_socket.closed is False
-    assert initialized_socket_interface.control_socket.closed is False
-
-    initialized_socket_interface.close()
-
-    assert initialized_socket_interface.pull_socket.closed is True
-    assert initialized_socket_interface.control_socket.closed is True
+    socket_interface.connect_data()
+    socket_interface.connect_control()
+    yield socket_interface
+    socket_interface.close()
 
 
-def test_setup(initialized_socket_interface):
-    """ test if the setup method open the control and pull socket
+def test_close(endpoint_interface):
     """
-    assert isinstance(initialized_socket_interface.poller, zmq.Poller)
-
-    check_socket(initialized_socket_interface.pull_socket, zmq.PULL,
-                 initialized_socket_interface.pull_socket_address)
-    check_socket(initialized_socket_interface.control_socket, zmq.PAIR,
-                 initialized_socket_interface.control_socket_address)
-
-
-def test_push_connection(connected_interface):
-    """test if the push socket is open
-
+    Test if the close method close both the control and data sockets.
     """
-    check_socket(connected_interface.push_socket, zmq.PUSH, connected_interface.pull_socket_address)
+    assert endpoint_interface._data_socket is not None
+    assert endpoint_interface._data_socket.closed is False
+    assert endpoint_interface._control_socket is not None
+    assert endpoint_interface._control_socket.closed is False
+
+    endpoint_interface.close()
+
+    assert endpoint_interface._data_socket is None
+    assert endpoint_interface._control_socket is None
 
 
-def test_push_disconnection(connected_interface):
-    """test if the disconnect method close the push socket
-
+def test_setup(endpoint_interface):
     """
-    assert connected_interface.push_socket.closed is False
-    connected_interface.close()
-    assert connected_interface.push_socket.closed is True
-
-
-def test_push_receive(connected_interface):
-    """test to send and receive a message from the push/pull socket
-
+    Test if the setup method opens the control and data sockets.
     """
-    msg = 'toto'
-    connected_interface.send_data(msg)
-    assert connected_interface.receive() == msg
+    assert isinstance(endpoint_interface._sockets_poller, zmq.Poller)
+
+    check_socket(endpoint_interface._data_socket, zmq.PULL, endpoint_interface.data_socket_filepath)
+    check_socket(endpoint_interface._control_socket, zmq.PAIR, endpoint_interface.control_socket_filepath)
 
 
-def test_control_connection(controlled_interface):
-    """test if the control socket is open
-
+def test_data_connect(endpoint_interface, data_peer_interface):
     """
-    check_socket(controlled_interface.control_socket, zmq.PAIR,
-                 controlled_interface.control_socket_address)
-
-
-def test_control_disconnection(controlled_interface):
-    """test if the disconnect method close the control socket
-
+    Test if the data socket is open.
     """
-    assert controlled_interface.control_socket.closed is False
-    controlled_interface.close()
-    assert controlled_interface.control_socket.closed is True
+    check_socket(data_peer_interface._data_socket, zmq.PUSH, data_peer_interface.data_socket_filepath)
 
 
-def test_control_receive(controlled_interface):
-    """test to send and receive a message from the control socket
-
+def test_data_disconnect(endpoint_interface, data_peer_interface):
     """
-    msg = 'toto'
-    controlled_interface.send_control(msg)
-    assert controlled_interface.receive() == msg
-
-
-def test_multiple_receive(fully_connected_interface):
-    """test to send and receive a message from the control and the push/pull
-    socket
-
+    Test if the disconnect method closes the data socket.
     """
-    controlled_msg = 'controlled_msg'
-    push_msg = 'push_msg'
-    fully_connected_interface.send_control(controlled_msg)
-    assert fully_connected_interface.receive() == controlled_msg
+    assert data_peer_interface._data_socket is not None
+    assert data_peer_interface._data_socket.closed is False
 
-    fully_connected_interface.send_control(push_msg)
-    assert fully_connected_interface.receive() == push_msg
+    data_peer_interface.close()
+
+    assert data_peer_interface._data_socket is None
+
+
+def test_data_receive(endpoint_interface, data_peer_interface):
+    """
+    Test to send and receive a message from the data socket.
+    """
+    msg = 'test-data-msg'
+    data_peer_interface.send_data(msg)
+    recv_msg = data_peer_interface.receive()
+    assert recv_msg == msg
+
+
+def test_data_send_not_connected(socket_interface):
+    """
+    Test that sending a message to a disconnected data socket raises an error.
+    """
+    with pytest.raises(NotConnectedException):
+        socket_interface.send_data('test-data-msg')
+
+
+def test_control_connect(endpoint_interface, control_peer_interface):
+    """
+    Test if the control socket is open.
+    """
+    check_socket(control_peer_interface._control_socket, zmq.PAIR, control_peer_interface.control_socket_filepath)
+
+
+def test_control_disconnect(endpoint_interface, control_peer_interface):
+    """
+    Test if the disconnect method closes the control socket.
+    """
+    assert control_peer_interface._control_socket is not None
+    assert control_peer_interface._control_socket.closed is False
+
+    control_peer_interface.close()
+
+    assert control_peer_interface._control_socket is None
+
+
+def test_control_send_not_connected(socket_interface):
+    """
+    Test that sending a message to a disconnected control socket raises an error.
+    """
+    with pytest.raises(NotConnectedException):
+        socket_interface.send_control('test-control-msg')
+
+
+def test_control_receive(endpoint_interface, control_peer_interface):
+    """
+    Test to send and receive a message from the control socket.
+    """
+    msg = 'test-control-msg'
+    control_peer_interface.send_control(msg)
+    recv_msg = control_peer_interface.receive()
+    assert recv_msg == msg
+
+
+def test_control_receive_not_connected(socket_interface):
+    """
+    Test that trying to receive a message from a disconnected control socket raises an error.
+    """
+    with pytest.raises(NotConnectedException):
+        socket_interface.receive_control()
+
+
+def test_control_receive_timeout(control_peer_interface):
+    """
+    Test reaching the timeout when trying to receive a message from the control socket.
+    """
+    msg = control_peer_interface.receive_control(timeout=100)
+    assert msg is None
+
+
+def test_multiple_receive(endpoint_interface, peer_interface):
+    """
+    Test to send and receive a message from both the control and the data sockets.
+    """
+    control_msg = 'test-control-msg'
+    peer_interface.send_control(control_msg)
+    recv_control_msg = endpoint_interface.receive()
+    assert recv_control_msg == control_msg
+
+    data_msg = 'test-data-msg'
+    peer_interface.send_data(data_msg)
+    recv_data_msg = peer_interface.receive()
+    assert recv_data_msg == data_msg
+
+
+def test_multiple_receive_not_connected(socket_interface):
+    """
+    Test that trying to receive a message from a disconnected socket interface raises an error.
+    """
+    with pytest.raises(NotConnectedException):
+        socket_interface.receive()
+
+
+def test_multiple_receive_timeout(endpoint_interface):
+    """
+    Test reaching the timeout when trying to receive a message from both the control and the data sockets.
+    """
+    msg = endpoint_interface.receive()
+    assert msg is None
