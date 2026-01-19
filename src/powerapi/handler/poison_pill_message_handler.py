92f1798485bc0322a73c6a67f54b660dcc334db8
Guillaume Fieni <guillaume.fieni@inria.fr>
2026-01-19 10:47:20 +0100

    refactor(handler): Cleanup poison pill message handler

--- src/powerapi/handler/poison_pill_message_handler.py
+++ src/powerapi/handler/poison_pill_message_handler.py
@@ -28,49 +28,30 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from powerapi.actor.message import PoisonPillMessage
-from powerapi.exception import UnknownMessageTypeException
 from .handler import Handler
 
 
 class PoisonPillMessageHandler(Handler):
     """
-    Generic handler for PoisonPillMessage
+    Handler responsible for shutting down an actor in response to a PoisonPillMessage.
     """
 
-    def teardown(self, soft=False):
+    def teardown(self, soft: bool = False) -> None:
         """
-        function called before terminating the actor process
-        could be redefined
+        Perform cleanup actions before the actor is stopped.
+        Subclasses may override this method to release resources or perform other shutdown-related tasks.
+        :param soft: Whether the shutdown is graceful (True) or immediate (False).
         """
 
-    def handle_msg(self, msg):
+    def handle(self, msg: PoisonPillMessage) -> None:
         """
-        Handle the given message
-        :param msg: The message to handle
+        Trigger the shutdown of the actor.
+        Pending messages will be processed before shutting down the actor if the graceful flag is enabled.
+        :param msg: Message received by the actor
         """
-        Handler.delegate_message_handling(self, msg)
-
-    def _empty_mail_box(self):
-        while True:
-            self.state.actor.socket_interface.timeout = 0.1
-            msg = self.state.actor.socket_interface.receive()
-
-            if msg is not None:
-                self.handle_msg(msg)
-            else:
-                return
-
-    def handle(self, msg):
-        """
-        Set the :attr:`alive <powerapi.actor.state.State.alive>`
-        attribute of the actor state to False
-
-        :param Object msg: the message received by the actor
-        """
-        if not isinstance(msg, PoisonPillMessage):
-            raise UnknownMessageTypeException(type(msg))
-
         if msg.is_soft:
-            self._empty_mail_box()
+            while (msg := self.state.actor.socket_interface.receive(timeout=100)) is not None:
+                self.handle_message(msg)
+
         self.teardown(soft=msg.is_soft)
         self.state.alive = False
